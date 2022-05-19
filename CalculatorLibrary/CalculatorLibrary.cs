a44911f64c5811e790a7f2ec9ebfcf80ca1a48fd
Eldiiar Dyushenaliev <scardes93@gmail.com>
2022-05-19 13:50:06 +0600

    Log file of calculation

--- CalculatorLibrary/CalculatorLibrary.cs
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.IO;
+using System.Diagnostics;
 
 namespace CalculatorLibrary
 {
     public class Calculator
     {
-        public static double DoOperation(double num1, double num2, string op)
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculator.log");
+            Trace.Listeners.Add(new TextWriterTraceListener(logFile));
+            Trace.AutoFlush = true;
+            Trace.WriteLine("Starting Calculator Log");
+            Trace.WriteLine(String.Format("Started{0}", System.DateTime.Now.ToString()));
+        }
+        public double DoOperation(double num1, double num2, string op)
         {
             double result = double.NaN;
 
@@ -12,18 +22,22 @@ namespace CalculatorLibrary
             {
                 case "+":
                     result = num1 + num2;
+                    Trace.WriteLine(String.Format("{0} + {1} = {2}", num1, num2, result));
                     break;
                 case "-":
                     result = num1 - num2;
+                    Trace.WriteLine(String.Format("{0} - {1} = {2}", num1, num2, result));
                     break;
                 case "*":
                     result = num1 * num2;
+                    Trace.WriteLine(String.Format("{0} * {1} = {2}", num1, num2, result));
                     break;
                 case "/":
                     // Ask the user to enter a non-zero divisor
                     if (num2 != 0)
                     {
                         result = num1 / num2;
+                        Trace.WriteLine(String.Format("{0} / {1} = {2}", num1, num2, result));
                     }
                     break;
                 //Return text for an incorrect option entry.
